20fbe84bc89944039d04bdeff50a39227c2d6a23
joe lun <40223237@gmail.com>
2015-04-26 22:31:31 +0800

    123

--- wsgi.py
+++ wsgi.py
@@ -549,12 +549,13 @@ class Hello(object):
     n_g2 = 18
     n_g3 = 20
     n_g4 = 28
-
+    n_g5 = 30
     # 計算兩齒輪的節圓半徑
     rp_g1 = m*n_g1/2
     rp_g2 = m*n_g2/2
     rp_g3 = m*n_g3/2
     rp_g4 = m*n_g4/2
+    rp_g5 = m*n_g5/2
     # 將第1齒輪順時鐘轉 90 度
     # 使用 ctx.save() 與 ctx.restore() 以確保各齒輪以相對座標進行旋轉繪圖
     ctx.save()
@@ -598,6 +599,16 @@ class Hello(object):
     # put it back
     ctx.translate(-(400+rp_g1+rp_g2+rp_g2+rp_g3+rp_g3+rp_g4),-400)
     spur.Spur(ctx).Gear(400+rp_g1+rp_g2+rp_g2+rp_g3+rp_g3+rp_g4,400,rp_g4,n_g4, pa, "purple")
+    ctx.restore()
+    # 第5齒
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(400+rp_g1+rp_g2+rp_g2+rp_g3+rp_g3+rp_g4+rp_g4+rp_g5,400)
+    # rotate to engage
+    ctx.rotate(-pi/2)
+    # put it back
+    ctx.translate(-(400+rp_g1+rp_g2+rp_g2+rp_g3+rp_g3+rp_g4+rp_g4+rp_g5),-400)
+    spur.Spur(ctx).Gear(400+rp_g1+rp_g2+rp_g2+rp_g3+rp_g3+rp_g4+rp_g4+rp_g5,400,rp_g5,n_g5, pa, "green")
     ctx.restore()
 
     </script>
